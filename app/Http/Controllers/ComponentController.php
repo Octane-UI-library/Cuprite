d010ba8a045fb3f8443021b1917f25ed91dbaf60
Smakouz Illea <illeasmakouz@gmail.com>
2025-01-30 23:52:59 +0200

    Pagginate category prev

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +10,18 @@ class ComponentController extends Controller
 {
     public function index()
     {
+//        $categories = Category::with(['components' => function ($query) {
+//            $query->take(4);
+//        }])->paginate(3);
+//
+//        return Inertia::render('Components', [
+//            'categories' => $categories,
+//            'currentPage' => $categories->currentPage(),
+//            'lastPage' => $categories->lastPage(),
+//            'total' => $categories->total(),
+//            'perPage' => $categories->perPage(),
+//        ]);
+
         return Inertia::render('Components');
     }
 
