db2cdfa451edec096f1269992f6d1f0ed0a084c0
Smakouz Illea <illeasmakouz@gmail.com>
2025-02-05 00:04:27 +0200

    Create Layout Tree

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard',[
+        return view('admin.dashboard.dashboard',[
             'user' => auth()->user(),
             'activeCategories' => Category::query()->count(),
             'activeComponents' => Component::query()->count(),
